229cdf3775b4e1bec28f629f638efcd7beb6f668
bramandityo <freez.meinster@gmail.com>
2025-08-06 14:28:50 +0700

    Wed Aug  6 02:28:50 PM WIB 2025

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 build:
 	@go build -o dist/nila-agent-dev
+run:
+	@$(MAKE) build
+	@./dist/nila-agent-dev dist/config
 release:
 	@GOOS=linux GOARCH=amd64 go build -o dist/nila-agent-linux
 	@GOOS=freebsd GOARCH=amd64 go build -o dist/nila-agent-freebsd
--- main.go
+++ main.go
@@ -1,45 +1,9 @@
 package main
 
-import (
-	"log"
-
-	"golang.org/x/crypto/ssh"
-)
+import "log"
 
 func main() {
-	// Replace with your SSH server details
-	sshAddress := "192.168.0.11:22"
-	username := "root"
-	password := "m" // Or use a private key for authentication
-
-	config := &ssh.ClientConfig{
-		User: username,
-		Auth: []ssh.AuthMethod{
-			ssh.Password(password),
-		},
-		// InsecureHostKeyCallback is for development only; for production,
-		// you should verify the host key.
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	}
-
-	conn, err := ssh.Dial("tcp", sshAddress, config)
-	if err != nil {
-		log.Fatalf("Failed to dial: %v", err)
-	}
-	defer conn.Close()
-
-	log.Println("SSH connection established successfully!")
-
-	// Example: Run a command
-	session, err := conn.NewSession()
-	if err != nil {
-		log.Fatalf("Failed to create session: %v", err)
-	}
-	defer session.Close()
-
-	output, err := session.CombinedOutput("ls -l")
-	if err != nil {
-		log.Fatalf("Failed to run command: %v", err)
-	}
-	log.Printf("Command output:\n%s", output)
+	cmd := "vmadm list"
+	out, err := sendSSH(cmd)
+	log.Printf("Command out : %s => Error %s", out, err)
 }
